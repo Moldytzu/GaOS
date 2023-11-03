58cc67e12a7b8f8c5fee2c1637ea926916d320e9
Moldytzu <m.alex123@yahoo.com>
2023-11-03 17:31:22 +0200

    framebuffer: display characters

--- kernel/src/devices/framebuffer/framebuffer.c
+++ kernel/src/devices/framebuffer/framebuffer.c
@@ -2,11 +2,28 @@
 #include <devices/serial/serial.h>
 #include <boot/limine.h>
 
+#define PSF2_MAGIC "\x72\xB5\x4A\x86"
+
+typedef struct __attribute__((packed))
+{
+    uint8_t magic[4];
+    uint32_t version;
+    uint32_t header_size;
+    uint32_t flags;
+    uint32_t length;
+    uint32_t glyph_size;
+    uint32_t height;
+    uint32_t width;
+} psf2_header_t;
+
+// globals
 bool framebuffer_available = false;
 framebuffer_t main_framebuffer;
 
 extern uint8_t _binary_kfont_psf_start[];
-const uint8_t *kfont_ptr = (uint8_t *)_binary_kfont_psf_start;
+static const uint8_t *kfont_ptr = (uint8_t *)_binary_kfont_psf_start;
+static psf2_header_t *font;
+static uint8_t *font_glyphs;
 
 bool framebuffer_generate_structure_from_limine(framebuffer_t *f)
 {
@@ -24,6 +41,21 @@ bool framebuffer_generate_structure_from_limine(framebuffer_t *f)
     return true;
 }
 
+void framebuffer_plot_character(char c, size_t x, size_t y, uint32_t colour)
+{
+    uint8_t *character = (uint8_t *)((uint64_t)font_glyphs + c * font->glyph_size);
+    uint8_t line_pitch = font->glyph_size / font->height;
+
+    for (uint32_t yy = 0; yy < font->height; yy++)
+    {
+        for (uint32_t xx = 0; xx < font->width; xx++)
+        {
+            if ((character[yy * line_pitch + xx / bitsof(uint8_t)] & (0b10000000 >> xx % bitsof(uint8_t))))
+                framebuffer_plot_pixel(x + xx, y + yy, colour);
+        }
+    }
+}
+
 void framebuffer_init()
 {
     // generate the framebuffer structure
@@ -33,9 +65,22 @@ void framebuffer_init()
         return;
     }
 
+    if (main_framebuffer.bpp != 32)
+    {
+        serial_send_string("framebuffer: unsupported bit-depth\n");
+        return;
+    }
+
+    // validate font
+    font = (psf2_header_t *)kfont_ptr;
+    font_glyphs = (uint8_t *)((uint64_t)font + font->header_size);
+    if (memcmp(font->magic, PSF2_MAGIC, 4) != 0)
+    {
+        serial_send_string("framebuffer: failed to validate the embeded font\n");
+        return;
+    }
+
     framebuffer_available = true;
 
-    // plot a pink line
-    for (int i = 0; i < 300; i++)
-        framebuffer_plot_pixel(i, i, 0xFF00FF);
+    framebuffer_plot_character('A', 0, 0, 0xFFFF00); // plot an 'A'
 }
--- kernel/src/misc/libc.h
+++ kernel/src/misc/libc.h
@@ -8,6 +8,7 @@
 
 #define iasm asm volatile
 #define ifunc static inline __attribute__((always_inline))
+#define bitsof(type) (sizeof(type) * 8)
 
 void *memcpy(void *dest, const void *src, size_t n);
 void *memset(void *s, int c, size_t n);
