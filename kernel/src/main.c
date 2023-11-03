00b2e91f23dbac9b462331342a5ad5fac3596b9b
Moldytzu <m.alex123@yahoo.com>
2023-11-03 11:48:16 +0200

    main: initialise serial on startup

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -1,11 +1,16 @@
 #include <misc/libc.h>
 #include <boot/limine.h>
 
+// devices
+#include <devices/serial/serial.h>
+
 // this is the entry point of the kernel
 void _start(void)
 {
+    serial_init(); // initialise serial interface
     limine_init(); // initialise limine protocol
 
+    // draw a line on screen
     struct limine_framebuffer *framebuffer = kernel_framebuffer_request.response->framebuffers[0];
     for (size_t i = 0; i < 100; i++)
     {
