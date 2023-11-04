1d980dbe29d61dc603b173a042721dde2316ba97
Moldytzu <m.alex123@yahoo.com>
2023-11-04 21:17:34 +0200

    x86_64: move simd in a folder

--- kernel/src/arch/x86_64/arch.h
+++ kernel/src/arch/x86_64/arch.h
@@ -1,8 +1,8 @@
 #pragma once
 #include <misc/libc.h>
-#include <arch/x86_64/pio.h>     // I/O ports
-#include <arch/x86_64/simd.h>    // SSE/AVX
-#include <arch/x86_64/smp/smp.h> // SMP
+#include <arch/x86_64/pio.h>       // I/O ports
+#include <arch/x86_64/simd/simd.h> // SSE/AVX
+#include <arch/x86_64/smp/smp.h>   // SMP
 
 ifunc void arch_hint_spinlock()
 {
