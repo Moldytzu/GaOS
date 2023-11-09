191fa30168cb7847d04df70c4d80010446801764
Moldytzu <m.alex123@yahoo.com>
2023-11-09 19:52:32 +0200

    x86_64/smp: remove old bootstrap code

--- kernel/src/arch/x86_64/arch.c
+++ kernel/src/arch/x86_64/arch.c
@@ -7,14 +7,8 @@
 #include <memory/physical/page_allocator.h>
 #include <boot/limine.h>
 
-uint64_t arch_trampoline_base;
-
 void arch_init()
 {
-    arch_trampoline_base = (uint64_t)page_allocate(1) - kernel_hhdm_offset;
-    if (arch_trampoline_base + PAGE > 1 * 1024 * 1024)
-        panic("Too high trampoline base. 0x%p > 1 MB", arch_trampoline_base);
-
     arch_load_gdt();
     arch_table_manager_init();
     arch_xapic_init(true);
