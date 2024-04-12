96affc88079fe639326f14c9ef3ca76832b4c2a3
Moldytzu <m.alex123@yahoo.com>
2024-04-12 21:54:40 +0300

    libk: implement strncmp

--- kernel/src/misc/libc.c
+++ kernel/src/misc/libc.c
@@ -76,6 +76,21 @@ int memcmp(const void *s1, const void *s2, size_t n)
     return 0;
 }
 
+int strncmp(const char *s1, const char *s2, size_t n)
+{
+    while (n && *s1 && (*s1 == *s2))
+    {
+        ++s1;
+        ++s2;
+        --n;
+    }
+
+    if (n == 0)
+        return 0;
+    else
+        return (*(unsigned char *)s1 - *(unsigned char *)s2);
+}
+
 void strrev(char *str)
 {
     size_t len = strlen(str);
--- kernel/src/misc/libc.h
+++ kernel/src/misc/libc.h
@@ -18,7 +18,9 @@ void *memset(void *s, int c, size_t n);
 void *memmove(void *dest, const void *src, size_t n);
 int memcmp(const void *s1, const void *s2, size_t n);
 
+int strncmp(const char *s1, const char *s2, size_t n);
 size_t strlen(char *str);
+void strrev(char *str);
 
 void vprintk(const char *fmt, va_list list);
 void vprintk_serial(const char *fmt, va_list list);
