e90cd6b82a812de2e43c67e9650fff6d18af7bd9
Moldytzu <m.alex123@yahoo.com>
2024-04-17 21:01:33 +0300

    panic: add a stack walk function

--- kernel/src/misc/panic.c
+++ kernel/src/misc/panic.c
@@ -3,6 +3,15 @@
 #include <devices/framebuffer/framebuffer.h>
 #include <arch/arch.h>
 
+#define STACK_TRACE_WALK(x) (__builtin_extract_return_addr(__builtin_return_address(x)))
+
+// displays the address if possible
+#define PRINT_TRACE_IF_POSSIBLE(x)                           \
+    if ((uint64_t)STACK_TRACE_WALK(x) <= kernel_hhdm_offset) \
+        halt();                                              \
+    printk("Trace %d: 0x%p\n", x, STACK_TRACE_WALK(x));      \
+    printk_serial("Trace %d: 0x%p\n", x, STACK_TRACE_WALK(x));
+
 noreturn void panic(const char *fmt, ...)
 {
     arch_kill_ap();
@@ -26,6 +35,16 @@ noreturn void panic(const char *fmt, ...)
     va_start(list, fmt);
     vprintk_serial(fmt, list);
     va_end(list);
+
+    /// print stack trace
+    printk("\n");
+    printk_serial("\n");
+    PRINT_TRACE_IF_POSSIBLE(1);
+    PRINT_TRACE_IF_POSSIBLE(2);
+    PRINT_TRACE_IF_POSSIBLE(3);
+    PRINT_TRACE_IF_POSSIBLE(4);
+    PRINT_TRACE_IF_POSSIBLE(5);
+    PRINT_TRACE_IF_POSSIBLE(6);
 
     halt();
 }
