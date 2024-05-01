d80b21c5b021e8d880ce19459fcc7ca0fcbdf341
Moldytzu <m.alex123@yahoo.com>
2024-05-01 23:17:51 +0300

    syscalls: add useful helper macros

---
+++ kernel/src/syscalls/helpers.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <misc/libc.h>
+#include <syscalls/syscalls.h>
+#include <schedulers/task/task.h>
+
+#define GET_CALLER_TASK() (((scheduler_context_t *)arch_get_scheduler_context())->running.current)
+#define GET_PAGE_TABLE(task) ((arch_page_table_t *)(task->state.cr3 + kernel_hhdm_offset))
+#define IS_MAPPED(x, page_table) (arch_table_manager_translate_to_physical(page_table, (uint64_t)(x)) != 0)
+#define IS_HIGHER_HALF_ADDRESS(x) ((uint64_t)x >= kernel_hhdm_offset)
