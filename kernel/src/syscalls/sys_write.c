e8eca0cbf73368434692c1b27d6a76f5bc38bd68
Moldytzu <m.alex123@yahoo.com>
2024-05-01 23:18:03 +0300

    sys_write: initial iteration of the syscall

--- kernel/src/syscalls/sys_write.c
+++ kernel/src/syscalls/sys_write.c
@@ -1,9 +1,25 @@
+#define MODULE "sys_write"
+#include <misc/logger.h>
 
-#include <misc/libc.h>
-#include <syscalls/syscalls.h>
+#include <syscalls/helpers.h>
 
-uint64_t sys_write(uint64_t num, uint64_t param1, uint64_t param2, uint64_t param3, uint64_t param4, uint64_t param5)
+int64_t sys_write(uint64_t num, uint64_t fd, char *buffer, size_t size)
 {
-    printk_serial("%p: %p %p %p %p %p\n", num, param1, param2, param3, param4, param5);
-    return 1;
+    (void)num, (void)fd;
+    scheduler_task_t *caller = GET_CALLER_TASK();
+    arch_page_table_t *caller_pt = GET_PAGE_TABLE(caller);
+
+    if (IS_HIGHER_HALF_ADDRESS(buffer) || !IS_MAPPED(buffer, caller_pt)) // userspace pointers are lower half and mapped
+    {
+        log_error("failed to write invalid pointer %p from %s", buffer, caller->name);
+        return -1;
+    }
+
+    for (size_t i = 0; i < size; i++)
+    {
+        printk("%c", buffer[i]);
+        printk_serial("%c", buffer[i]);
+    }
+
+    return 0;
 }
