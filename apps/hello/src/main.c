5f2e91360a7891825c786693cb0258f6aa62a92b
Moldytzu <m.alex123@yahoo.com>
2024-05-01 23:18:45 +0300

    hello: first userspace hello world

--- apps/hello/src/main.c
+++ apps/hello/src/main.c
@@ -1,10 +1,12 @@
 #include <stdint.h>
 
-uint64_t _syscall(uint64_t number, uint64_t param1, uint64_t param2, uint64_t param3, uint64_t param4, uint64_t param5);
+#define SYS_WRITE 0
+
+int64_t _syscall(uint64_t number, uint64_t param1, uint64_t param2, uint64_t param3, uint64_t param4, uint64_t param5);
 
 int _start()
 {
-    _syscall(0, 1, 2, 3, 4, 5);
+    _syscall(SYS_WRITE, 0, (uint64_t) "Hello, Gallium!", 15, 0, 0);
     while (1)
         ;
     return 0;
