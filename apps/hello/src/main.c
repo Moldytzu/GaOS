b0de33c6f3217fe8170e1847e6790e60c03286c4
Moldytzu <m.alex123@yahoo.com>
2024-05-30 21:00:48 +0300

    hello: open a file

--- apps/hello/src/main.c
+++ apps/hello/src/main.c
@@ -1,12 +1,30 @@
 #include <stdint.h>
+#include <stddef.h>
 
 #define SYS_WRITE 0
+#define SYS_OPEN 1
 
 int64_t _syscall(uint64_t number, uint64_t param1, uint64_t param2, uint64_t param3, uint64_t param4, uint64_t param5);
 
+int64_t sys_write(uint64_t fd, char *buffer, size_t size)
+{
+    return _syscall(SYS_WRITE, fd, (uint64_t)buffer, (uint64_t)size, 0, 0);
+}
+
+int64_t sys_open(char *filename, uint64_t mode)
+{
+    return _syscall(SYS_OPEN, (uint64_t)filename, mode, 0, 0, 0);
+}
+
 int _start()
 {
-    _syscall(SYS_WRITE, 0, (uint64_t) "Hello, Gallium!", 15, 0, 0);
+    sys_write(0, "Hello, Gallium!", 15); // write a message
+
+    // open a file
+    int64_t fd = sys_open("/initrd/test.txt", 0);
+    char fd_str[] = {'0' + fd, 0};
+    sys_write(0, fd_str, 1);
+
     while (1)
         ;
     return 0;
