f26ee7584d5278881895643b87b836282e8c0c7d
Moldytzu <m.alex123@yahoo.com>
2024-05-30 20:52:36 +0300

    syscalls/sys_open: open syscall

---
+++ kernel/src/syscalls/sys_open.c
@@ -0,0 +1,36 @@
+#define MODULE "sys_write"
+#include <misc/logger.h>
+
+#include <syscalls/helpers.h>
+
+int64_t sys_open(uint64_t num, char *filename, uint64_t mode)
+{
+    used(num), used(filename), used(mode);
+
+    scheduler_task_t *caller = GET_CALLER_TASK();
+    arch_page_table_t *caller_pt = GET_PAGE_TABLE(caller);
+
+    // sanitize address
+    if (IS_HIGHER_HALF_ADDRESS(filename) || !IS_MAPPED(filename, caller_pt)) // userspace pointers are lower half and mapped
+    {
+        log_error("failed to open invalid pointer %p from %s", filename, caller->name);
+        return -EPERM;
+    }
+
+    // open the node while propagating the errors
+    vfs_fs_node_t *node = vfs_open(filename, mode);
+    if (is_error(node))
+        return error_of(node);
+
+    // push the node on the translation table
+    size_t fd = caller->fd_count++;
+
+    if (caller->fd_count > caller->fd_max)
+    {
+        // todo: reallocate
+    }
+
+    caller->fd_translation[fd] = node;
+
+    return fd;
+}
--- kernel/src/syscalls/syscalls.c
+++ kernel/src/syscalls/syscalls.c
@@ -1,7 +1,11 @@
 #include <syscalls/syscalls.h>
 #define DECLARE_SYSCALL(name) void name()
 
-uint64_t syscall_count = 1;
+uint64_t syscall_count = 2;
 
 DECLARE_SYSCALL(sys_write);
-void (*syscall_handlers[])() = {sys_write};
+DECLARE_SYSCALL(sys_open);
+void (*syscall_handlers[])() = {
+    sys_write,
+    sys_open,
+};
