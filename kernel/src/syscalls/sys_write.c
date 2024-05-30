ebfed6e57c657d9ccdcd51cb049a07bbacee8f9c
Moldytzu <m.alex123@yahoo.com>
2024-05-30 20:52:58 +0300

    syscalls/sys_write: return error instead of magic number

--- kernel/src/syscalls/sys_write.c
+++ kernel/src/syscalls/sys_write.c
@@ -9,12 +9,16 @@ int64_t sys_write(uint64_t num, uint64_t fd, char *buffer, size_t size)
     scheduler_task_t *caller = GET_CALLER_TASK();
     arch_page_table_t *caller_pt = GET_PAGE_TABLE(caller);
 
+    // todo: use fd
+
+    // sanitize address
     if (IS_HIGHER_HALF_ADDRESS(buffer) || !IS_MAPPED(buffer, caller_pt)) // userspace pointers are lower half and mapped
     {
         log_error("failed to write invalid pointer %p from %s", buffer, caller->name);
-        return -1;
+        return -EPERM;
     }
 
+    // write the buffer to kernel log
     for (size_t i = 0; i < size; i++)
     {
         printk("%c", buffer[i]);
