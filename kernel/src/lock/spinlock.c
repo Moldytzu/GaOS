d3443dd89d898bb9d32f3118feef23638ce008cf
Moldytzu <m.alex123@yahoo.com>
2024-12-15 17:17:15 +0200

    spinlock: add deadlock detection for debugging

--- kernel/src/lock/spinlock.c
+++ kernel/src/lock/spinlock.c
@@ -1,17 +1,24 @@
 #include <lock/spinlock.h>
 #include <arch/arch.h>
+#include <stdatomic.h>
 
 void spinlock_acquire(spinlock_t *lock)
 {
-    while (1)
+    int count = 0;
+    while (atomic_flag_test_and_set_explicit(lock, memory_order_acquire))
     {
-        if (__sync_bool_compare_and_swap(lock, 0, 1))
-            break;
         arch_hint_spinlock();
+
+        count++;
+        if (count >= 100000000)
+        {
+            printk_serial_unsafe("deadlock to return to %p with interrupts %s\n", STACK_TRACE_WALK(0), (arch_interrupts_enabled() ? "enabled" : "disabled"));
+            count = 0;
+        }
     }
 }
 
 void spinlock_release(spinlock_t *lock)
 {
-    __atomic_store_n(lock, 0, __ATOMIC_SEQ_CST);
+    atomic_flag_clear_explicit(lock, memory_order_release);
 }
