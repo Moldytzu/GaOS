65450136e1ad2a62289c242db83ce47d54f82492
Moldytzu <m.alex123@yahoo.com>
2025-01-01 22:19:11 +0200

    sys_read: replace log_error with trace_error

--- kernel/src/syscalls/sys_read.c
+++ kernel/src/syscalls/sys_read.c
@@ -13,18 +13,24 @@ int64_t sys_read(uint64_t num, uint64_t fd, char *buffer, size_t size)
     // verify pointer
     if (!IS_USER_MEMORY(buffer, caller))
     {
-        log_error("failed to read invalid pointer %p from %s", buffer, caller->name);
+        trace_error("failed to read invalid pointer %p from %s", buffer, caller->name);
         return -EPERM;
     }
 
     // verify fd
     if (fd >= caller->fd_count)
+    {
+        trace_error("fd %d of %s is invalid", fd, caller->name);
         return -EBADF;
+    }
 
     vfs_fs_node_t *node = caller->fd_translation[fd];
 
     if (node == nullptr)
+    {
+        trace_error("fd %d of %s is invalid", fd, caller->name);
         return -EBADF;
+    }
 
     // make sure the size doesn't overflow the fd
     if (node->seek_position + size > node->max_seek_position)
