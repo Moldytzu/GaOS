465bde3f13d4aa58d6d5096d30bc518090f9441b
Moldytzu <m.alex123@yahoo.com>
2025-01-04 22:05:13 +0200

    x86_64: move PUSH/POP_REG macros to an include file

--- kernel/src/arch/x86_64/state/state.asm
+++ kernel/src/arch/x86_64/state/state.asm
@@ -2,45 +2,7 @@ bits 64
 
 section .text
 
-%macro PUSH_REG 0
-    push r15
-    push r14
-    push r13
-    push r12
-    push r11
-    push r10
-    push r9
-    push r8
-    push rbp
-    push rdi
-    push rsi
-    push rdx
-    push rcx
-    push rbx
-    push rax
-    mov rax, cr3
-    push rax
-%endmacro
-
-%macro POP_REG 0
-    pop rax
-    mov cr3, rax
-    pop rax
-    pop rbx
-    pop rcx
-    pop rdx
-    pop rsi
-    pop rdi
-    pop rbp
-    pop r8
-    pop r9
-    pop r10
-    pop r11
-    pop r12
-    pop r13
-    pop r14
-    pop r15
-%endmacro
+%include "registers.mac"
 
 global arch_switch_state, arch_save_simd_state, arch_restore_simd_state
 
